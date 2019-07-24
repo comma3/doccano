ad69e03a72b2c02027247c29efa668eb531acff6
mattfred <matthewfred@gmail.com>
2019-07-24 13:54:18 -0600

    remove other startup command

--- tools/run.sh
+++ tools/run.sh
@@ -4,6 +4,6 @@ set -o errexit
 
 if [[ ! -d "app/staticfiles" ]]; then python app/manage.py collectstatic --noinput; fi
 
-python app/manage.py wait_for_db
+
 
 gunicorn --bind="0.0.0.0:${PORT:-8000}" --workers="${WORKERS:-1}" --pythonpath=app app.wsgi --timeout 300
